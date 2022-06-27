b310b91d996f61361625d1d4a9eb5b35ae5e15fa
Jonathan <jonathansilvabatista@gmail.com>
2022-06-27 09:43:22 -0300

    Atualização

--- Projetos de Phyton/Aula 3.1.py
+++ Projetos de Phyton/Aula 3.1.py
@@ -1,8 +1,8 @@
-a = int(input("numero 1: "))
-b  = int(input("numero 2: "))
-resto = a % 2
-resto_b = b % 2
-if resto == 0 or not resto_b > 0:
+a=int(input("numero 1: "))
+b=int(input("numero 2: "))
+resto=a % 2
+resto_b=b % 2
+if resto== 0 or not resto_b > 0:
     print("é par")
 else:
     print("não foi digitado numero par")
